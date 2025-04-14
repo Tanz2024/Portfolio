030d73743de1240c3dfefc2b3d511968f214b93a
unknown <tanzimbinzahir2019@gmail.com>
2025-04-15 01:41:51 +0800

    hide api key

--- my-react-app/src/firebaseConfig.js
+++ my-react-app/src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuWLS8wXIXipzfcmiQxMXxOhXeD4UtAzE",
-  authDomain: "tanzimportfolio.firebaseapp.com",
-  projectId: "tanzimportfolio",
-  storageBucket: "tanzimportfolio.appspot.com", // fix typo here
-  messagingSenderId: "428868166100",
-  appId: "1:428868166100:web:90c574926a64c541147641",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
